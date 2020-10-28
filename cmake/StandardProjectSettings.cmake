44a0de476f6df5c148e6440431dbcd9e05bcade1
helixhamin <helixhamin5@hotmail.com>
2020-10-28 22:58:11 +0800

    Update to fix cmake issues

--- cmake/StandardProjectSettings.cmake
+++ cmake/StandardProjectSettings.cmake
@@ -19,19 +19,19 @@ set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
 
-if(ENABLE_IPO)
-  include(CheckIPOSupported)
-  check_ipo_supported(
-    RESULT
-    result
-    OUTPUT
-    output)
-  if(result)
-    set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
-  else()
-    message(SEND_ERROR "IPO is not supported: ${output}")
-  endif()
-endif()
+# if(ENABLE_IPO)
+  # include(CheckIPOSupported)
+  # check_ipo_supported(
+    # RESULT
+    # result
+    # OUTPUT
+    # output)
+  # if(result)
+    # set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+  # else()
+    # message(SEND_ERROR "IPO is not supported: ${output}")
+  # endif()
+# endif()
 
 if(MSVC)
     list(APPEND FLAGS_AND_DEFINES
